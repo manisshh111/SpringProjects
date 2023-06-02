2dbdc460b280bac70a736d895cb58cf1ad4a1eca
Manish <manish.sharma22221@gmail.com>
2023-06-02 16:18:05 +0530

    directly calling methods of parent using arrow functions

--- React_tutorial/todo-app/src/Components/Counter/CounterButton.jsx
+++ React_tutorial/todo-app/src/Components/Counter/CounterButton.jsx
@@ -4,23 +4,12 @@ import {PropTypes} from 'prop-types'
 
 export default function CounterButton({by, incrementMethod, decrementMethod}){
 
-
-
-    function incrementCounterFunction(){
-        incrementMethod(by);
-    }
-
-    function decrementCounterFunction(){
-        decrementMethod(by);
-    }
-
-
     return(
         <div className="Counter">
 
-         <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
-         <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
-
+         <button className="counterButton" onClick={()=> incrementMethod(by)}>+{by}</button>    //directly calling methods of Parent comp using arror functions
+         <button className="counterButton" onClick={()=> decrementMethod(by)}>-{by}</button>
+         
 
 
         </div>
